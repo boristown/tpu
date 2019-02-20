bbb4b0e7572a4f5a83e9cd61cda54fb4271ebe44
boristown <boristown@gmail.com>
2019-02-20 11:00:05 +0800

    新建python2_install.bat

--- python2_install.bat
+++ python2_install.bat
@@ -2,7 +2,5 @@ python -m pip install --upgrade pip
 python -m pip install google-cloud
 pip install google-cloud-storage
 pip install absl-py
-pip install tensorflow==1.12
-pip install numpy --upgrade
 pip install apache-beam --upgrade
 pause
