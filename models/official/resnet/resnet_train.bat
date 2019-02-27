e4e9a2517d211826e32f03185537af6e62acc48b
boristown <boristown@gmail.com>
2019-02-27 08:49:58 +0800

    更新训练数据

--- models/official/resnet/resnet_train.bat
+++ models/official/resnet/resnet_train.bat
@@ -1,11 +1,11 @@
 python resnet_main.py ^
---train_steps=20 ^
---train_batch_size=6 ^
---eval_batch_size=6 ^
---num_train_images=6 ^
---num_eval_images=6 ^
---steps_per_eval=1 ^
---iterations_per_loop=1 ^
+--train_steps=4720592 ^
+--train_batch_size=50 ^
+--eval_batch_size=50 ^
+--num_train_images=4720592 ^
+--num_eval_images=127306 ^
+--steps_per_eval=5 ^
+--iterations_per_loop=5 ^
 --resnet_depth=50 ^
 --use_tpu=False ^
 --data_dir="G:\TPU\data" ^
