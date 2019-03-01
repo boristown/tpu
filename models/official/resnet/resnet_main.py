ea9e7c776367f603a0ae59e295253ba86ea374a8
Boris Town <47687653+boristown@users.noreply.github.com>
2019-03-01 16:27:44 +0800

    Update resnet_main.py

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -532,7 +532,7 @@ def resnet_model_fn(features, labels, mode, params):
         A dict of the metrics to return from evaluation.
       """
       tf.logging.info("logits=%s,labels=%s" % (logits.shape, labels.shape))
-      label = np.reshape(label, [label.shape[0]/FLAGS.num_parallel_calls,label.shape[1]*FLAGS.num_parallel_calls])
+      labels = np.reshape(labels, [labels.shape[0]/FLAGS.num_parallel_calls,labels.shape[1]*FLAGS.num_parallel_calls])
       predictions = tf.argmax(logits, axis=1)
       labels_top_1 = tf.argmax(labels, axis=0)
       # top_1_accuracy = tf.metrics.accuracy(labels, predictions)
