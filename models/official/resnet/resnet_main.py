c35b9ecc3bcbc006a56b6334956a70bbb471b5a3
Boris Town <47687653+boristown@users.noreply.github.com>
2019-03-02 10:41:29 +0800

    Update resnet_main.py

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -538,7 +538,7 @@ def resnet_model_fn(features, labels, mode, params):
       labels_reshaped = tf.reshape(labels, [logits.shape[0],logits.shape[1]])
       predictions = tf.argmax(logits, axis=1)
       #labels_top_1 = tf.argmax(labels, axis=0)
-      labels_top_1 = tf.argmax(labels, axis=1)
+      labels_top_1 = tf.argmax(labels_reshaped, axis=1)
       # top_1_accuracy = tf.metrics.accuracy(labels, predictions)
       top_1_accuracy = tf.metrics.accuracy(labels_top_1, predictions)
       
