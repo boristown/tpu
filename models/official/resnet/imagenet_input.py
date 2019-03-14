af414565edb283bd9431c653bc6772bb312f9cb2
Boris Town <47687653+boristown@users.noreply.github.com>
2019-03-14 08:12:45 +0800

    Update imagenet_input.py

--- models/official/resnet/imagenet_input.py
+++ models/official/resnet/imagenet_input.py
@@ -380,7 +380,8 @@ class ImageNetInput(ImageNetTFExampleInput):
 
     # Shuffle the filenames to ensure better randomization.
     file_pattern = os.path.join(
-      self.data_dir, 'train-*' if self.is_training else 'validation-*')
+    #  self.data_dir, 'train-*' if self.is_training else 'validation-*')
+      self.data_dir, 'train-*')
     #raise Exception(f'file_pattern = {filename} in class ImageNetInput')
     # For multi-host training, we want each hosts to always process the same
     # subset of files.  Each host only sees a subset of the entire dataset,
