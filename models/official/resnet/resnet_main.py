0b5d110266383b2d177ce2b3ff6d0ce461cde0a4
SoleTrader <47381390+soletpu@users.noreply.github.com>
2019-03-16 08:22:55 +0800

    Fix prediction sump bug

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -404,7 +404,7 @@ def resnet_model_fn(features, labels, mode, params):
     logits = tf.cast(logits, tf.float32)
   elif FLAGS.precision == 'float32':
     logits = build_network()
-  tf.logging.info("features=%s,labels=%s,logits=%s" % (features.shape, labels.shape, logits.shape))
+  #tf.logging.info("features=%s,labels=%s,logits=%s" % (features.shape, labels.shape, logits.shape))
   #logits = tf.transpose(logits, [1, 0, 2])  # NCL to CNL
   if mode == tf.estimator.ModeKeys.PREDICT:
     predictions = {
