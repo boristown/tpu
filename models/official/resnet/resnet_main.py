0b65a0b5d2910f275951bb884d26a54682f1ea7d
boristown <boristown@gmail.com>
2019-04-02 15:53:06 +0800

    os.rename(price_file, price_file_new)

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -861,7 +861,7 @@ def main(unused_argv):
           for price_file in price_files:
             tf.logging.info('Removing ' + price_file)
             price_file_new = price_file.replace("price-", "backup-")
-            os.price_file(price_file, price_file_new)
+            os.rename(price_file, price_file_new)
             
     if FLAGS.export_dir is not None and FLAGS.mode != 'predict':
       # The guide to serve a exported TensorFlow model is at:
