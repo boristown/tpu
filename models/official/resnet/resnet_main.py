43117cae4d954ef43f71842364e7c4165758fe22
Boris Town <47687653+boristown@users.noreply.github.com>
2019-04-21 18:17:16 +0800

    Update resnet_main.py

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -262,7 +262,7 @@ flags.DEFINE_integer('image_size', 4, 'The input image size.')
 
 flags.DEFINE_string(
     #'dropblock_groups', '5,6,7,8',
-    'dropblock_groups', '7,8',
+    'dropblock_groups', '6,7,8',
     help=('A string containing comma separated integers indicating ResNet '
           'block groups to apply DropBlock. `3,4` means to apply DropBlock to '
           'block groups 3 and 4. Use an empty string to not apply DropBlock to '
@@ -272,7 +272,7 @@ flags.DEFINE_float(
     help=('keep_prob parameter of DropBlock. Will not be used if '
           'dropblock_groups is empty.'))
 flags.DEFINE_integer(
-    'dropblock_size', default=3,
+    'dropblock_size', default=1,
     help=('size parameter of DropBlock. Will not be used if dropblock_groups '
           'is empty.'))
 
