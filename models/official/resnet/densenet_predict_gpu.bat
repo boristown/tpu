6f91d6ffc6d09a4829f3c9ab7305ac2dc7c5d8c4
boristown <boristown@gmail.com>
2019-08-18 20:04:39 +0800

    创建densenet_predict_gpu.bat

--- models/official/resnet/densenet_predict_gpu.bat
+++ models/official/resnet/densenet_predict_gpu.bat
@@ -1,13 +1,13 @@
 python resnet_main.py ^
 --mode=predict ^
---train_steps=7000 ^
+--train_steps=11800 ^
 --train_batch_size=100 ^
 --eval_batch_size=100 ^
 --num_train_images=529 ^
 --num_eval_images=529 ^
 --steps_per_eval=100 ^
 --iterations_per_loop=100 ^
---resnet_depth=169 ^
+--resnet_depth=121 ^
 --use_tpu=False ^
 --data_dir="G:\TPU\data" ^
 --prices_dir="G:\TPU\prices" ^
