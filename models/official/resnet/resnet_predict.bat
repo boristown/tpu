f89443d7a70f3cbdb8b1bb900a4a6bbd17a3b734
SoleTrader <47381390+soletpu@users.noreply.github.com>
2019-08-27 17:23:42 +0800

    Update resnet_predict.bat

--- models/official/resnet/resnet_predict.bat
+++ models/official/resnet/resnet_predict.bat
@@ -1,13 +1,13 @@
 python resnet_main.py ^
 --mode=predict ^
---train_steps=7000 ^
+--train_steps=26400 ^
 --train_batch_size=100 ^
 --eval_batch_size=100 ^
 --num_train_images=100 ^
 --num_eval_images=100 ^
 --steps_per_eval=100 ^
 --iterations_per_loop=100 ^
---resnet_depth=121 ^
+--resnet_depth=169 ^
 --use_tpu=False ^
 --data_dir="C:\TPU\data" ^
 --prices_dir="C:\TPU\prices" ^
