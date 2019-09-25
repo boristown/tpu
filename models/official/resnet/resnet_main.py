7214b9b6b02270032f5c9f21c407bbfadf23dd61
全王 <47687653+boristown@users.noreply.github.com>
2019-09-25 14:29:53 +0800

    AdamOptimizer

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -498,6 +498,7 @@ def resnet_model_fn(features, labels, mode, params):
     
     # LARS is a large batch optimizer. LARS enables higher accuracy at batch 16K
     # and larger batch sizes.
+    '''
     if FLAGS.train_batch_size >= 16384 and FLAGS.enable_lars:
       learning_rate = 0.0
       optimizer = lars_util.init_lars_optimizer(current_epoch)
@@ -511,7 +512,7 @@ def resnet_model_fn(features, labels, mode, params):
     # I think Adam optimizer is better than LARS/Momentum optimizer for ZeroAI
     # Boris Town 20190207
     optimizer = tf.train.AdamOptimizer()
-    '''
+    
     
     if FLAGS.use_tpu:
       # When using TPU, wrap the optimizer with CrossShardOptimizer which
