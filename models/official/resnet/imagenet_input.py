675b59237d8e1ac6aa2a0ebcdcc9268cd75d0ae2
AI纪元 <47687653+boristown@users.noreply.github.com>
2020-01-12 19:16:35 +0800

    tf.sparse.to_dense

--- models/official/resnet/imagenet_input.py
+++ models/official/resnet/imagenet_input.py
@@ -206,8 +206,8 @@ class ImageNetTFExampleInput(object):
     prices = prices_features['prices']
     label = label_features['label']
     
-    label = tf.convert_to_tensor(label)
-    prices = tf.convert_to_tensor(prices)
+    label = tf.sparse.to_dense(label)
+    prices = tf.sparse.to_dense(prices)
     tf.logging.info("prices=%s,labels=%s" % (prices,label))
     
     label = tf.reshape(label, [-1])
