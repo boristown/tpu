56a74b721245a3a16675c358bba318412f9af86c
AI Era <47687653+boristown@users.noreply.github.com>
2020-01-13 18:05:30 +0800

    bfloat16

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -484,7 +484,10 @@ def resnet_model_fn(features, labels, mode, params):
         trainingInputSet = tf.concat([trainingInputSet, trainingInputData],axis=0)
         trainingInputSet = tf.concat([trainingInputSet, trainingInputData*-1.0+1.0],axis=0)
         #LabelData = tf.cond(tf.greater_equal(priceList[trainingIndex+priceInputCount], priceList[trainingIndex+priceInputCount-1]), lambda: tf.Variable(tf.constant([[0 ,1]])), lambda: tf.Variable(tf.constant([[1 ,0]])))
-        LabelData = tf.cond(tf.greater_equal(priceList[trainingIndex+priceInputCount], priceList[trainingIndex+priceInputCount-1]), lambda: tf.constant([[0.0 ,1.0]]), lambda: tf.constant([[1.0 ,0.0]]))
+        if FLAGS.precision == 'bfloat16':
+          LabelData = tf.cond(tf.greater_equal(priceList[trainingIndex+priceInputCount], priceList[trainingIndex+priceInputCount-1]), lambda: tf.constant([[0.0 ,1.0]], dtype=bfloat16), lambda: tf.constant([[1.0 ,0.0]], dtype=bfloat16))
+        else:
+          LabelData = tf.cond(tf.greater_equal(priceList[trainingIndex+priceInputCount], priceList[trainingIndex+priceInputCount-1]), lambda: tf.constant([[0.0 ,1.0]], dtype=float32), lambda: tf.constant([[1.0 ,0.0]], dtype=float32))
         #LabelData = [[0, 1]] if priceList[trainingIndex+priceInputCount] >= priceList[trainingIndex+priceInputCount-1] else [[1, 0]]
         LabelSet = tf.concat([LabelSet, LabelData], axis=0)
         LabelSet = tf.concat([LabelSet, LabelData*-1+1], axis=0)
