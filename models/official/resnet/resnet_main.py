ea4dd8c4cfc89602b42be51791e2e8d07091fc3b
Boris Town <boristown@outlook.com>
2020-03-18 07:03:27 +0800

    loss = cross_entropy + FLAGS.weight_decay * tf.add_n([

--- models/official/resnet/resnet_main.py
+++ models/official/resnet/resnet_main.py
@@ -599,7 +599,8 @@ def resnet_model_fn(features, labels, mode, params):
       onehot_labels=labels,
       label_smoothing=FLAGS.label_smoothing)
 
-  loss = cross_entropy + params['weight_decay'] * tf.add_n([
+  #loss = cross_entropy + params['weight_decay'] * tf.add_n([
+  loss = cross_entropy + FLAGS.weight_decay * tf.add_n([
         tf.nn.l2_loss(v)
         for v in tf.trainable_variables()
         if 'batch_normalization' not in v.name
