717dc830fd0135a2ae5f8871c3ea2101c2b991f9
全王 <47687653+boristown@users.noreply.github.com>
2020-09-19 18:00:13 +0800

    turtle5

--- models/official/resnet/export_model.bat
+++ models/official/resnet/export_model.bat
@@ -1,19 +1,19 @@
 python resnet_main.py ^
 --mode=save_model ^
---train_steps=26400 ^
+--train_steps=36400 ^
 --train_batch_size=100 ^
 --eval_batch_size=100 ^
---num_train_images=100 ^
---num_eval_images=100 ^
+--num_train_images=2000 ^
+--num_eval_images=2000 ^
 --steps_per_eval=100 ^
 --iterations_per_loop=100 ^
 --resnet_depth=169 ^
 --use_tpu=False ^
---data_dir="C:\TPU\data" ^
---prices_dir="C:\TPU\prices" ^
---predict_dir="C:\TPU\predict" ^
---model_dir="C:\TPU\model" ^
---export_dir="D:/saved_model/" ^
+--data_dir="D:\TPU\data" ^
+--prices_dir="D:\Robot\WX_BG\Output\prices" ^
+--predict_dir="D:\Robot\WX_BG\Output\predict" ^
+--model_dir="D:\TPU\model" ^
+--export_dir="D:/saved_model/turtle5" ^
 --tpu="" ^
 --precision="float32" ^
 --data_format="channels_last"
