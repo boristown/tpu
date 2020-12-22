858b059dfcc10eb8af1f091b55500783968e99ad
全王 <47687653+boristown@users.noreply.github.com>
2020-12-22 20:14:55 +0800

    Create export_model_second.bat

---
+++ models/official/resnet/export_model_second.bat
@@ -0,0 +1,20 @@
+python resnet_main.py ^
+--mode=save_model ^
+--train_steps=336400 ^
+--train_batch_size=100 ^
+--eval_batch_size=100 ^
+--num_train_images=2000 ^
+--num_eval_images=2000 ^
+--steps_per_eval=100 ^
+--iterations_per_loop=100 ^
+--resnet_depth=201 ^
+--use_tpu=False ^
+--data_dir="D:\TPU\data" ^
+--prices_dir="D:\Robot\WX_BG\Output\prices_second" ^
+--predict_dir="D:\Robot\WX_BG\Output\predict_second" ^
+--model_dir="D:\TPU\model_second" ^
+--export_dir="D:/saved_model/turtle7" ^
+--tpu="" ^
+--precision="float32" ^
+--data_format="channels_last"
+pause
